3b0ade1bb34297e648ca630b8b298f266cf722c2
Julian Chan Palomo <73038870+JulianChan123@users.noreply.github.com>
2023-04-30 02:04:56 -0600

    disenioInfuncional

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+export function App(){
+    return(
+    <section className='App'>
+        <TwitterFollowCard name="Julian Chan" userName="midudev"/>
+        <TwitterFollowCard name="Julian Chan" userName="midudev"/>
+    </section>
+    )
+}
